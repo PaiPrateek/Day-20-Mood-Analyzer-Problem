f77c0f5d19dd4d84134fdbe30151c442bc952199
Prateek Pai <paiprateek123@gmail.com>
2022-07-09 12:25:57 +0530

    Calling Analyse Mood function with message as parameter and return Happy or Sad Mood

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -13,7 +13,17 @@ namespace TestProject
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            //Act
+            string expected = "SAD";
+            string message = "I am in sad Mood";
+
+            MoodAnalyse moodAnalyse = new MoodAnalyse(message);
+
+            //Arrange
+            string mood = moodAnalyse.AnalyseMood();
+
+            //Assert
+            Assert.AreEqual(expected,mood);
         }
     }
 }
