803b52857fa649e222d799194e62fcd2900f12f4
Prateek Pai <paiprateek123@gmail.com>
2022-07-09 12:38:37 +0530

    Given “I am in Sad

--- MoodAnalyser/Program.cs
+++ MoodAnalyser/Program.cs
@@ -7,6 +7,11 @@ namespace MoodAnalyser
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Mood Analyser Problem");
+            Console.WriteLine("\n");
+
+            string message = "I am in sad Mood";
+            MoodAnalyse moodAnalyse = new MoodAnalyse(message);
+            moodAnalyse.AnalyseMood();
         }
     }
 }
